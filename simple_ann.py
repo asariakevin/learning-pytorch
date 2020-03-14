3fa416114af6f80d08a40a43f45b75ac98aad512
asariakevin <asariakevin@gmail.com>
2020-03-14 09:39:11 +0300

    rename layers to fc syntax

--- simple_ann.py
+++ simple_ann.py
@@ -9,17 +9,17 @@ class Network(nn.Module):
         super().__init__()
 
         #inputs to hidden layer a Linear transformation
-        self.hidden = nn.Linear(784,256)
+        self.fc1 = nn.Linear(784,256)
 
         #output layer,10 unit i.e one for each digit
-        self.output = nn.Linear(256,10)
+        self.fc2 = nn.Linear(256,10)
 
 
     def forward(self,x):
 
         # hidden layer with sigmoid activation
-        x = F.sigmoid(self.hidden(x))
-        x = F.softmax(self.output(x),dim=1)
+        x = F.sigmoid(self.fc1(x))
+        x = F.softmax(self.fc2(x),dim=1) #don't forget the dim=1 argument
 
         return x
 
